21a660d01586c40d157ca80fe10961af2024f835
Lê Quang Anh <lequanganhh1624@gmail.com>
2025-03-31 08:30:39 +0700

    Lưu thay đổi trước khi chuyển nhánh

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ce58dbbd8e252cd61bf412e66dcfa1ec179a4a7e9fbb7c59d9b9fe6e30726d6
-size 23679
+oid sha256:9b2278bf73e6e683ef3f93a7be7680ec1a3c4109f5ba65299b80f46c532ca970
+size 25792
